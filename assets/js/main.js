0715fe93f77c1b320750bc5ba498f885d7f4993e
Ratkai <pratkai@3c063050d7ec.ant.amazon.com>
2022-07-13 15:05:19 +0100

    add content

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,4 +260,22 @@
     }
   });
 
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+        this.innerHTML = "See more..."
+
+      } else {
+        content.style.display = "block";
+        this.innerHTML = "See less";
+      }
+    });
+} 
+
 })()
